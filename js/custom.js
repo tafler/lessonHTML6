765fbe34ddc43c2d9b235646de8297052c7f4adb
tafler <taffik@yandex.ru>
2018-03-06 10:21:11 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+  
+
+    $(document).ready(function(){
+    $('.main-slider-content').slick({
+		dots: true,
+		infinite: true,
+		autoplay: true,
+		arrows: false,
+		autoplaySpeed: 7000,
+		fade: true,
+		cssEase: 'linear'
+});
+    });
+
+    $(document).ready(function(){
+    $('.comment-slider').slick({
+		dots: true,
+		infinite: true,
+		autoplay: true,
+		arrows: false,
+});
+    });
+
+
